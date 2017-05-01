18588f386e7b105d44b6144bde0803c19f2c3830
ZhouWei <weichou2010@gmail.com>
2017-05-01 17:29:55 +0800

    upd |> update documents of Keep.

--- src/hobby/wei/c/anno/proguard/Keep.java
+++ src/hobby/wei/c/anno/proguard/Keep.java
@@ -23,27 +23,68 @@ import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
- * <i>弱保留</i> 本注解<u>直接作用</u>的各种名称。<br>
- * 使用范围：任何位置。<br>
+ * <i>Weak Keep</i> the names this annotation <u>act directly</u> on. What it means, see below.<br>
+ * Scope: anywhere possible.<br>
  * <p>
- * 名词解释：<br>&nbsp;&nbsp;
- * <i>强</i>保留：既不被重命名，又不被删除；<br>&nbsp;&nbsp;
- * <i>弱</i>保留：只确保不被重命名，不保证不被删除；<br>&nbsp;&nbsp;
- * <font color=red><i>反</i></font>保留：不保证不被重命名，但确保不被删除；<br>&nbsp;&nbsp;
- * <i>不</i>保留：排除到以上保留名单之外。<br>
- * 优先级：<br>&nbsp;&nbsp;
- * 由高到低依次为强、反、弱、不。被延续到子类（接口）的，子类享有相同优先级。<br>&nbsp;&nbsp;
- * 任何位置，若被不同优先级的注解同时标注，则会应用较高优先级。<br>
- * 约定：<br>&nbsp;&nbsp;
- * 含有<font color=red><i>$$</i></font> 字符的为<i>反保留</i>；<br>&nbsp;&nbsp;
- * 含有<font color=red><i>$</i></font> 字符（除反保留外）的为<i>强保留</i>；<br>&nbsp;&nbsp;
- * 不含以上标识字符的为<i>弱保留</i>；<br>&nbsp;&nbsp;
- * 没有用于<i>不保留</i> 的标签，即：不加任何标注则为不保留；<br>&nbsp;&nbsp;
- * 另：以<font color=red><i>e</i></font> 结尾的表示将其前面名称表达的功能<i>延续到子类（接口）</i>。
- * 需要注意的是：只有父类（接口）存在的情况下，才能延续到子类。即：首先需要将父类强保留或反保留。<br>
- * 其它：<br>&nbsp;&nbsp;
- * 对于<i>inject</i> 包中的任何注解，只要该注解没有被优化掉，就会对其直接作用的内容进行反保留。
- * <p>&nbsp;&nbsp;
+ * Explain the Glossary:
+ * <ul>
+ * <li><i>Strong</i> Keep: neither be renamed, nor be deleted;
+ * <li><i>Weak</i> Keep: guarantee not be renamed, but without guarantee not be deleted;
+ * <li><font color=red><i>Reverse</i></font> Keep: without guarantee not be renamed, but guarantee not be deleted;
+ * <li><i>Non</i> Keep: Out of the rules above. Means apply proguard default.
+ * </ul>
+ * Priority:
+ * <ul>
+ * From high to low in the order: <i>Strong</i>, <i>Weak</i>, <i>Reverse</i>, <i>Non</i>.
+ * When be carried over into subclass (subinterface), subclass got the same priority.<br>
+ * Anywhere, if one name be annotated by some different priority but same function of "@KeepXx"s,
+ * the higher one will be applied.
+ * </ul>
+ * Appoint:
+ * <ul>
+ * <li>Contain chars <font color=red><i>$$</i></font> means <i>Reverse Keep</i>;
+ * <li>Contain char <font color=red><i>$</i></font> means <i>Strong Keep</i> (exclude Reverse Keep);
+ * <li>Does not contain chars above means <i>Weak Keep</i>;
+ * <li>Does not exist annotation definition of <i>Non Keep</i>, means no need for annotate names;
+ * <li>End with <font color=red><i>e</i></font> means the function indicated by the words before "e"
+ * will be <i>carried over into subclass (subinterface)</i>.
+ * Note that only when superclass (superinterface) did not deleted on proguard task, subclass (subinterface) may kept.
+ * Means must Strong/Reverse Keep superclass (superinterface) first.
+ * </ul>
+ * Else:
+ * <ul>
+ * Any annotation in package <i>{@code **.anno.inject}</i>, if not be deleted on proguard task,
+ * will apply Reverse Keep on the names it act directly on.
+ * </ul>
+ * <p>
+ * <i>弱保留</i> 本注解<u>直接作用</u>的各种名称。<br>
+ * 使用范围：任何可用的位置。<br>
+ * <p>
+ * 名词解释：
+ * <ul>
+ * <li><i>强</i>保留：既不被重命名，又不被删除；
+ * <li><i>弱</i>保留：只确保不被重命名，不保证不被删除；
+ * <li><font color=red><i>反</i></font>保留：不保证不被重命名，但确保不被删除；
+ * <li><i>不</i>保留：排除到以上保留名单之外。即应用混淆。
+ * </ul>
+ * 优先级：
+ * <ul>
+ * 由高到低依次为强、反、弱、不。被延续到子类（接口）的，子类享有相同优先级。<br>
+ * 任何位置，若被不同优先级的注解同时标注，则会应用较高优先级。
+ * </ul>
+ * 约定：
+ * <ul>
+ * <li>含有<font color=red><i>$$</i></font> 字符的为<i>反保留</i>；
+ * <li>含有<font color=red><i>$</i></font> 字符的为<i>强保留</i>（反保留除外）；
+ * <li>不含以上标识字符的为<i>弱保留</i>；
+ * <li>没有用于<i>不保留</i> 的标签，即：不加任何标注则为不保留；
+ * <li>另：以<font color=red><i>e</i></font> 结尾的表示将其前面名称表达的功能<i>延续到子类（接口）</i>。
+ * 需要注意的是：只有父类（接口）没有被混淆删除的情况下，才能延续到子类。即：首先需要将父类强保留或反保留。
+ * </ul>
+ * 其它：
+ * <ul>
+ * 对于包<i>{@code **.anno.inject}</i> 中的任何注解，只要该注解没有被优化掉，就会对其直接作用的内容进行反保留。
+ * </ul>
  *
  * @author Wei Chou(weichou2010@gmail.com)
  * @version 1.0, 11/12/2015
